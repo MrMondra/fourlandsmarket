778feca72ccd2b0d270a7af54e88a0b2e78fb1c4
MrMondra <mrmondra95@gmail.com>
2024-04-28 16:08:15 -0500

    fix: Fixed secrets of git actions

--- src/main/java/com/fourlandsmarket/FourlandsMarketApplication.java
+++ src/main/java/com/fourlandsmarket/FourlandsMarketApplication.java
@@ -9,8 +9,5 @@ public class FourlandsMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(FourlandsMarketApplication.class, args);
     }
-
-    public void sumas(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
-        int r = a + b + c + d + e + f + g + h + i + j;
-    }
+    
 }
