bc95c212d2be1fa01a84182d589548b3da2dca4c
MrMondra <mrmondra95@gmail.com>
2024-04-28 12:50:24 -0500

    fix: correction and validation of rules of project with checkstyle

--- src/main/java/com/fourlandsmarket/FourlandsMarketApplication.java
+++ src/main/java/com/fourlandsmarket/FourlandsMarketApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FourlandsMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FourlandsMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FourlandsMarketApplication.class, args);
+    }
 
-	public void sumas(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j){}
-
+    public void sumas(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
+        int r = a + b + c + d + e + f + g + h + i + j;
+    }
 }
